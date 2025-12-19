8581339abda8605133b005bd0ba888bf08fa1e03
Anukriti-j <anu200307@gmail.com>
2025-12-19 12:42:17 +0530

    Initial Commit

---
+++ DeepLinkDemo/DeepLinkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkDemoApp.swift
+//  DeepLinkDemo
+//
+//  Created by coditas on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
